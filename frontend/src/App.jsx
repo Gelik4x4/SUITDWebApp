c94c82a871b7f1797efd79c50c94df7da4b2ca5f
Gelik <113053733+Gelik4x4@users.noreply.github.com>
2026-03-13 00:20:49 +0300

    feat: create TestConnection component, move from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import { supabase } from './supabaseClient'
-
-
-
-function TestConnection() {
-  const [firstItem, setFirstItem] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchFirstItem() {
-      try {
-        // Запрос: берём первую запись из таблицы todos
-        const { data, error } = await supabase
-          .from('schedule')
-          .select('*')
-          .limit(1)
-          .single(); // .single() гарантирует, что вернётся один объект, а не массив
-
-        if (error) throw error;
-        setFirstItem(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchFirstItem();
-  }, []);
-
-  if (loading) return <div>Проверка подключения...</div>;
-  if (error) return <div style={{ color: 'red' }}>Ошибка: {error}</div>;
-
-  return (
-    <div style={{ padding: '20px', border: '1px solid green' }}>
-      <h3>✅ Подключение работает!</h3>
-      <p>Первый элемент в таблице <strong>todos</strong>:</p>
-      <pre>{JSON.stringify(firstItem, null, 2)}</pre>
-    </div>
-  );
-}
+import TestConnection from './components/db test/TestConnection'
 
 
 function Button({count}) {
