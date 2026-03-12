ce6b3f3dca698bb210f050b8b73971c7894a8621
Gelik <113053733+Gelik4x4@users.noreply.github.com>
2026-03-13 00:40:22 +0300

    feat: first test product version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,63 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import TestConnection from './components/db test/TestConnection'
-
-
-function Button({count}) {
-    return (       
-      <>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </>
-  )
-}
-
-function Button2({string}) {
-    return (       
-      <>
-        <button>
-            {string}
-        </button>
-      </>
-  )
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  const user = {
-      name: "John",
-      age: 30
-  };
-  console.log("1")
+
   return (
-    <>
-      <TestConnection/>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button count={count}/>
-        <Button2 string={user["name"]}/>
-        <Button2 string={user["age"]}/>      
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
