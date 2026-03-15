2d8c4f8ad2ef2192451c46b98d2e7156a7f09545
Gelik <113053733+Gelik4x4@users.noreply.github.com>
2026-03-15 03:43:26 +0300

    feat: create constants file for navigation

---
+++ frontend/src/constants/navigation.js
@@ -0,0 +1,17 @@
+export const PAGE_TITLES = {
+  '/studentcard': 'Студенческий билет',
+  '/news': 'Новости',
+  '/teachers': 'Преподаватели',
+
+  '/sport': 'Спорт',
+  '/events': 'События',
+  '/askquestion': 'Задать вопрос',
+
+  '/contests': 'Конкурсы',
+  '/vacancies': 'Вакансии',
+  '/internships': 'Стажировки',
+  '/specialoffers': 'Специальные предложения',
+
+  '/podcasts': 'Подкасты',
+  '/articles': 'Статьи'
+};
