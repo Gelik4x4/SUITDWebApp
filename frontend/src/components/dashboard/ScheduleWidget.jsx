2b38c279cc786d2e7e3d8390e40971ab5eee06b3
Gelik4x4 <a.d.mironenko2003@gmail.com>
2026-03-22 00:50:01 +0500

    feat: add schedule styles

--- frontend/src/components/dashboard/ScheduleWidget.jsx
+++ frontend/src/components/dashboard/ScheduleWidget.jsx
@@ -36,7 +36,7 @@ export default function ScheduleWidget() {
           <span className="card__title">Расписание</span>
           <span className="card__subtitle"> · Четверг, 5 декабря</span>
         </div>
-        <button className="icon-btn"><IconArrow /></button>
+        <button className="icon-btn"><img src="/src/components/icons/arrow-btn.svg"/></button>
       </div>
       <div className="schedule-list">
         {scheduleData.map((item, i) => (
