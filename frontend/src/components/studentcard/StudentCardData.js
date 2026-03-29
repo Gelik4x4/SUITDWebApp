6bfdff093a7f012136377fc73900e3f96dd1b9d8
Gelik4x4 <a.d.mironenko2003@gmail.com>
2026-03-29 23:34:23 +0500

    upd: student name

--- frontend/src/components/studentcard/StudentCardData.js
+++ frontend/src/components/studentcard/StudentCardData.js
@@ -1,12 +1,12 @@
 /* Данные студенческого билета приходят с бэкенда  */
 export const STUDENT_DATA = {
   qualification: 'БАКАЛАВРИАТ',
-  name: 'Васильев Дмитрий Владимирович',
+  name: 'Трушкова Ангелина Денисовна',
   role: 'Студент',
-  group: '2-МД-5',
+  group: '4-МД-5',
   studentId: '222-22-22',
   birthDate: '19.07.2003',
   department: 'Цифровых и аддитивных технологий',
-  photo: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=200&h=200&fit=crop&face',
+  photo: '/src/components/img/student.jpg',
   barcode: '2222222222222',
 };
