b060490f6831659976172a1f98a31645ace35f94
Gelik4x4 <a.d.mironenko2003@gmail.com>
2026-04-25 02:08:45 +0300

    add: clubs data

---
+++ frontend/src/constants/clubsData.js
@@ -0,0 +1,44 @@
+export const CLUBS = [
+  {
+    id: 'nlo',
+    name: 'Инженерный клуб НЛО',
+    category: 'IT',
+    image: 'https://sun9-80.userapi.com/impf/ThxBlyCGA38O0QNnktaEsLQ6ILnKbg4Fs6x6yQ/obB_9AlT7wY.jpg?size=1920x768&quality=95&crop=0,0,1920,767&sign=d3178e0c8a20778cfaaac3eaf00933f0&c_uniq_tag=gDx6n1MY48SyUaM-GTeL_8NXgscpeHyDoKRBvsKCVH8&type=cover_group',
+    description: 'В клубе участники собирают роботов, разрабатывают электронные устройства, создают проекты с IoT и носимой электроникой. Мы помогаем разобраться в технике, предоставляем наставников, обучаем и поддерживаем лучшие идеи, в том числе финансово.',
+    address: 'Вознесенский проспект, 44-46, Санкт-Петербург',
+    website: 'https://vk.com/engin_club',
+    websiteLabel: 'vk.com/engin_club',
+  },
+  {
+    id: 'vr',
+    name: 'Клуб виртуальной реальности',
+    category: 'IT',
+    image: 'https://sutd.ru/images/news/2026/mitap-5.jpg',
+    description: 'Клуб VR-среды объединяет студентов, увлечённых технологиями виртуальной и дополненной реальности. Участники создают VR-проекты, изучают разработку под Unreal Engine и Unity, работают с VR-оборудованием и участвуют в хакатонах.',
+    address: 'Вознесенский проспект, 44-46, Санкт-Петербург',
+    website: 'https://t.me/vrsreda',
+    websiteLabel: 't.me/vrsreda',
+  },
+  {
+    id: 'respawn',
+    name: 'Сообщество создателей игр Респавн',
+    category: 'IT',
+    image: 'https://sun9-79.userapi.com/impf/yS6Kdipct8bMRepCX2nyfPVsGD0eNR6m4pS_0Q/gypK9-UxIO8.jpg?size=1920x768&quality=95&crop=0,0,1920,767&sign=dbaf5d7dea59927a1a0c1be6e8e693fd&c_uniq_tag=oMZAvOgjGV7ShvRJFiJ65EdAGC8V5773bTLSmKCrVOQ&type=cover_group',
+    description: 'Респавн — сообщество студентов-разработчиков игр СПбГУПТД. Мы проводим митапы, game jam\'ы, воркшопы по геймдизайну, программированию и 2D/3D-арту. Участники выпускают собственные игры и находят команды для совместных проектов.',
+    address: 'Санкт-Петербург, СПбГУПТД',
+    website: 'https://vk.com/respawn_hub',
+    websiteLabel: 'vk.com/respawn_hub',
+  },
+  {
+    id: 'otriad',
+    name: 'Штаб студенческих отрядов СПбГУПТД',
+    category: 'Другое',
+    image: 'https://sutd.ru/images/news/baltika-sso-1.jpg', // фото подгружается через sutd.ru
+    description: 'Штаб студенческих отрядов объединяет педагогические, строительные, сервисные и другие отряды университета. Участие в отрядах — это возможность заработать в студенческие годы, получить опыт командной работы и интересно провести лето.',
+    address: 'Вознесенский проспект, 44-46, Санкт-Петербург',
+    website: 'https://sutd.ru/stud-otryad/',
+    websiteLabel: 'sutd.ru/stud-otryad',
+  },
+];
+
+export const CLUB_CATEGORIES = ['Клубы кафедры ЦАТ', 'Дизайн', 'IT', 'VR', 'Другое'];
