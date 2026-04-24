f557e5ccdd64e676534bd42d5f7cc935f90ab748
Gelik4x4 <a.d.mironenko2003@gmail.com>
2026-04-25 02:23:28 +0300

    update: update styles

--- frontend/src/components/askquestion/ChatMessage.jsx
+++ frontend/src/components/askquestion/ChatMessage.jsx
@@ -1,28 +1,34 @@
-import Icon from '@icon/Icon';
 import './ChatMessage.css';
 
-function ChatMessage({ msg }) {
-  const isUser = msg.side === 'user';
+function BotAvatar() {
   return (
-    <div className={`chat-msg chat-msg--${msg.side}`}>
-      <div className="chat-msg__bubble">
-        {msg.text.split('\n').map((line, i, arr) => (
-          <span key={i}>
-            {line}
-            {i < arr.length - 1 && <br />}
-          </span>
-        ))}
-      </div>
-      <div className="chat-msg__meta">
-        {msg.time}
-        {isUser && (
-          <span className={`chat-msg__tick${msg.read ? ' chat-msg__tick--read' : ''}`}>
-            <Icon name="Tick"/>
-          </span>
-        )}
-      </div>
+    <div className="chat-msg__bot-avatar">
+      <img
+        src="/src/assets/img/bot-avatar.png"
+        alt=""
+        className="chat-msg__bot-photo"
+        onError={e => {
+          e.target.style.display = 'none';
+          e.target.nextSibling.style.display = 'flex';
+        }}
+      />
     </div>
   );
 }
 
-export default ChatMessage;
+export default function ChatMessage({ msg }) {
+  const isUser = msg.side === 'user';
+  return (
+    <div className={`chat-msg chat-msg--${msg.side}`}>
+      {!isUser && <BotAvatar />}
+      <div className="chat-msg__content">
+        <div className="chat-msg__bubble">
+          {msg.text.split('\n').map((line, i, arr) => (
+            <span key={i}>{line}{i < arr.length - 1 && <br />}</span>
+          ))}
+        </div>
+        <div className="chat-msg__time">{msg.time}</div>
+      </div>
+    </div>
+  );
+}
