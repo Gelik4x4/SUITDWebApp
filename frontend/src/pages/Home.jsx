17b0a5e530148b41ef7940e3e83227f970192d40
Gelik4x4 <a.d.mironenko2003@gmail.com>
2026-04-25 02:16:52 +0300

    update: styles

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import ScheduleWidget from "../components/dashboard/ScheduleWidget"
-import InternshipsWidget from "../components/dashboard/InternshipsWidget"
 import OffersWidget from "../components/dashboard/OffersWidget"
 import NewsWidget from "../components/dashboard/NewsWidget"
 import VacanciesWidget from "../components/dashboard/VacanciesWidget"
@@ -11,33 +10,33 @@ function Home() {
   return (
     <div className="grid">
 
-      <div className="grid-schedule">
-        <ScheduleWidget />
+      {/* Левая колонка: расписание + спецпредложения */}
+      <div className="grid-col--left">
+        <div className="grid-schedule">
+          <ScheduleWidget />
+        </div>
+        <div className="grid-offers">
+          <OffersWidget />
+        </div>
       </div>
 
-      <div className="grid-internships">
-        <InternshipsWidget />
-      </div>
-
-      <div className="grid-news">
-        <NewsWidget />
-      </div>
-
-      <div className="grid-offers">
-        <OffersWidget />
-      </div>
-
-      <div className="grid-vacancies">
-        <VacanciesWidget />
-      </div>
-
-      <div className="grid-events">
-        <EventsWidget />
+      {/* Центральная колонка: вакансии + новости (верхняя строка) и мероприятия (нижняя строка) */}
+      <div className="grid-col--center">
+        <div className="top-row">
+          <div className="grid-vacancies">
+            <VacanciesWidget />
+          </div>
+          <div className="grid-news">
+            <NewsWidget />
+          </div>
+        </div>
+        <div className="grid-events">
+          <EventsWidget />
+        </div>
       </div>
 
     </div>
   );
 }
-
 
 export default Home
